06448c9d25a9624bbed35fe3046a18625a5faa82
codebreaker1365 <117101354+codebreaker1365@users.noreply.github.com>
2022-11-01 00:11:39 +0530

    Add files via upload

---
+++ subset.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void  subset(int arr[],int n){
+    for (int i = 0; i < (1<<n); i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i & 1<<j){
+                cout<<arr[j]<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    
+}
+
+main(){
+    int n;
+    cout<<"enter size of array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"elements of array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    subset(arr,n);
+
+    return 0;
+
+}
