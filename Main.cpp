fd64088d929dae8a9a6b81bf1b567da7859f7bb6
Kush Munot <65546898+Kush-munot@users.noreply.github.com>
2022-11-01 00:08:24 +0530

    Codeforces Problem Solved

---
+++ Main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, flag = 0;
+    cin >> x;
+
+    for (int i = 1; i <= x; i++)
+    {
+        for (int j = 1; j <= x; j++)
+        {
+            if (i * j > x && i % j == 0 && i / j < x)
+            {
+                cout << i << " " << j << endl;
+                return 0;
+            }
+        }
+    }
+    cout << -1 << endl;
+}
